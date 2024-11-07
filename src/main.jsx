1742a4cdb88f3e5ad9ade0e2c4dbac40b3883cb8
texora <ultantaylor@gmail.com>
2024-11-08 05:14:03 +0900

    feat: integrate Tailwind CSS into React Vite project

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "./tailwindcss.css"
 import "./index.css";
 import "./i18n.js";
 
